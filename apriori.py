8eefcfc9acf777144925bbaa6e6aedff0fb1d8c3
Irwin Li <izl2000@columbia.edu>
2017-12-02 18:45:14 -0500

    Removed ungraded rows

--- apriori.py
+++ apriori.py
@@ -3,6 +3,9 @@ import sys
 import csv
 
 def read_data(filename, limit=1000):
+	"""
+	Yields a generator with the data
+	"""
 	with open(filename, newline='') as csvfile:
 		reader = csv.reader(csvfile, delimiter=",", quotechar='"')
 		i = 0
@@ -12,6 +15,9 @@ def read_data(filename, limit=1000):
 				continue
 			elif (i > limit + 1):
 				break
+			if row[-4] == '' or row[-4] == 'Not Yet Graded':
+				i -= 1
+				continue
 			yield row
 
 def apriori():
@@ -28,7 +34,7 @@ if __name__ == "__main__":
 	data = read_data(filename, limit=1000)
 
 	for d in data:
-		print(len(d))
+		pass
 
 	print("Filename:", filename)
 	print("Min support:", min_sup)
